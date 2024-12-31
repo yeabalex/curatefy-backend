117b321a98d07c5620e3d59dd231a718e5f775ae
Yab <yeabalex18@gmail.com>
2024-12-31 03:27:20 -0800

    redisenv

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,7 +1,7 @@
 const { createClient } = require("redis");
 
 const redisClient = createClient({
-  url: "redis://nileredis-0cxykn.serverless.usw2.cache.amazonaws.com:6379",
+  url: process.env.REDIS_CLIENT,
 });
 redisClient
   .connect()
