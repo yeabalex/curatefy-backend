852f10dd96caafa3cab8d65edd2682e507c8e78c
Yab <yeabalex18@gmail.com>
2024-12-31 07:12:02 -0800

    aacc

--- src/config/cors.config.js
+++ src/config/cors.config.js
@@ -1,16 +1,26 @@
 const corsOptions = (req, callback) => {
- const whitelist = ["http://localhost:3000", "https://curatefy.vercel.app"];
- const origin = req.header("Origin");
- if (!origin) {
-   return callback(null, { origin: false });
- }
- const isWhitelisted = whitelist.includes(origin);
- const options =  { 
-       origin: true, 
-       credentials: true,
-       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-       allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin']
-     }
-   
- callback(null, options);
+  const whitelist = ["http://localhost:3000", "https://curatefy.vercel.app"];
+  const origin = req.header("Origin");
+
+  if (!origin) {
+    return callback(null, { origin: false }); 
+  }
+
+  const isWhitelisted = whitelist.includes(origin);
+  const options = {
+    origin: isWhitelisted ? origin : false, 
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'X-Requested-With',
+      'Accept',
+      'Origin'
+    ],
+  };
+
+  callback(null, options);
 };
+
+module.exports = corsOptions;
